1580e345c9e1c7de40d7224f33cc783cbdfc3f08
Seasawher <tellagoodstory@icloud.com>
2025-10-06 20:50:52 +0900

    修正

--- lakefile.lean
+++ lakefile.lean
@@ -4,10 +4,7 @@ open Lake DSL
 package "ToySat" where
   version := v!"0.1.0"
 
+@[default_target]
 lean_lib «ToySat» where
   -- add library configuration options here
   globs := #[.submodules `ToySat]
-
-@[default_target]
-lean_exe "toysat" where
-  root := `Main
